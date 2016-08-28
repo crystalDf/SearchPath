14ed21224cb207d5a6f96879634187bb007c1c59
crystalDf <chendong333@gmail.com>
2016-08-28 09:08:28 +0800

    Search Path

--- src/com/star/search/SearchPath.java
+++ src/com/star/search/SearchPath.java
@@ -2,12 +2,6 @@ package com.star.search;
 
 public class SearchPath {
 
-    public static final int ROW = 5;
-    public static final int COLUMN = 11;
-
-    public static final String NO_PATH_EXISTS = "no path exists";
-    public static final String PATH = "Path:";
-
     public static final char[][] SWAMP = {
             {'*', '*', '.', '*', '.', '*', '.', '.', '.', '.', '*'},
             {'.', '.', '*', '.', '*', '.', '.', '.', '*', '*', '.'},
@@ -16,10 +10,18 @@ public class SearchPath {
             {'.', '.', '*', '.', '*', '.', '.', '.', '*', '.', '*'},
     };
 
+    public static final int ROW = SWAMP.length;
+    public static final int COLUMN = SWAMP[0].length;
+
+    public static final String PATH = "Path:";
+    public static final String NO_PATH_EXISTS = "no path exists";
+
     public static void main(String[] args){
         for (int i = 0; i < ROW; i++) {
             if (SWAMP[i][0] == '*') {
-                System.out.println(i + " " + searchPath(i, 0, " " + i));
+                System.out.println("Start at Row " + i + " " + searchPath(i, 0, " " + i));
+            } else {
+                System.out.println("Start at Row " + i + " " + NO_PATH_EXISTS);
             }
         }
     }
@@ -31,10 +33,9 @@ public class SearchPath {
         }
 
         for (int i = currentRow - 1; i <= currentRow + 1; i++) {
-
             if ((i >= 0) && (i < ROW) && SWAMP[i][currentColumn + 1] == '*') {
                 String pathResult = searchPath(i, currentColumn + 1, path + " " + i);
-                if (pathResult.contains(PATH)) {
+                if (!pathResult.equals(NO_PATH_EXISTS)) {
                     return pathResult;
                 }
             }
